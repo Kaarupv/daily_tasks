43e64e543b6a2b44088c2de210a8527e6c15d937
kaarup <vares.kaarup@gmail.com>
2024-09-15 00:12:07 +0300

    intial project setup

---
+++ src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Hello Kaarup, today is </h1>
+        <p>This is a simple homepage to get started.</p>
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+div {
+    text-align: center;
+    margin-top: 50px;
+}
+h1 {
+    color: #42b983;
+}
+</style>
